247c702eb5c0add502b5855eedbfd9ecb05cd374
YTheNoobGoda <davidespinhamarques@gmail.com>
2022-12-27 02:02:49 +0000

    eat_piece done

--- moxie.pl
+++ moxie.pl
@@ -89,3 +89,41 @@ distance(N1,N2):-
   N1 is N2 -1.
 distance(N1,N2):-
   N1 is N2 +1.
+
+%eat_piece(+C,+Row,+Col,+Row2,+Col2)
+eat_piece(C,Row,Col,Row2,Col2):-
+  check_pieces(Row,Col,Row2,Col2,Row3,Col3),
+  get_position(Row,Col,Num1),
+  !,
+  Num1 is C,
+  get_position(Row2,Col2,Num2),
+  !,
+  \+ Num1 is Num2,
+  board(B,N),
+  retract(board(B,N)),
+  replace(B,Row,Col,0,B2),
+  replace(B2,Row2,Col2,0,B3),
+  replace(B3,Row3,Col3,C,B4),
+  assert(board(B4,N)).
+  
+
+check_pieces(Row,Col,Row2,Col2,Row3,Col3):-
+  Row is Row2 +1,
+  Col is Col2 +1,
+  Row3 is Row2 -1,
+  Col3 is Col2 -1.
+check_pieces(Row,Col,Row2,Col2,Row3,Col3):-
+  Row is Row2 -1,
+  Col is Col2 -1,
+  Row3 is Row2 +1,
+  Col3 is Col2 +1.
+check_pieces(Row,Col,Row2,Col2,Row3,Col3):-
+  Row is Row2 +1,
+  Col is Col2 -1,
+  Row3 is Row2 -1,
+  Col3 is Col2 +1.
+check_pieces(Row,Col,Row2,Col2,Row3,Col3):-
+  Row is Row2 -1,
+  Col is Col2 +1,
+  Row3 is Row2 +1,
+  Col3 is Col2 -1.
